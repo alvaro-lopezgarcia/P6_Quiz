9604bf3a208e68dd78067756340680164ffb9927
alvaro <alvaro1332@gmail.com>
2018-06-06 20:05:48 +0200

    practica8

--- routes/index.js
+++ routes/index.js
@@ -146,5 +146,15 @@ router.delete('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)',
     quizController.adminOrAuthorRequired,
     tipController.destroy);
 
+//PRACTICA8
+router.get('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)/edit',
+    sessionController.loginRequired,
+    tipController.adminOrAuthorRequired,
+    tipController.edit);
+
+router.put('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)',
+    sessionController.loginRequired,
+    tipController.adminOrAuthorRequired,
+tipController.update);
 
 module.exports = router;
